c470d9b83b9221d4eaf472f8c978e1c0bc0fadc4
n.sai.surya.yalla <n.sai.surya.yalla@HDC4-L-138LW7Z.dir.svc.accenture.com>
2022-08-03 12:05:29 +0530

    updated controller

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -7,22 +7,13 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.EmployeeInput;
 import com.example.demo.service.EmployeeService;
 
 
 
-@RestController
+@Controller
 public class EmployeeController {
 	
 	@Autowired
